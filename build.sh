a1b8680973b1a80ca18db448e75414ec59693bae
Kevin Cai <kevincai96@gmail.com>
2017-11-06 11:08:33 -0500

    Improve build.sh process and update toolchain

--- build.sh
+++ build.sh
@@ -1,8 +1,16 @@
+echo "Setting env variables for pre-compile..."
+export CROSS_COMPILE="/home/synthlock/toolchains/aarch64-linaro-linux-android/bin/aarch64-linaro-linux-android-"
+export ARCH="arm64"
+echo "Sanity check:"
+echo $ARCH
+echo $CROSS_COMPILE
+echo
+
 echo "Initial building of dtbs..."
 echo
-make -j4 ARCH=arm64 CROSS_COMPILE=/home/synthlock/toolchains/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9/bin/aarch64-linux-android- lineage_tulip_defconfig dtbs
-
+make -j4 lineage_tulip_defconfig dtbs
 echo
+
 echo "Copying built dtbs from arm to arm64..."
 echo
 cp arch/arm/boot/dts/*.dtb arch/arm64/boot/dts/
@@ -10,12 +18,12 @@ cp arch/arm/boot/dts/*.dtb arch/arm64/boot/dts/
 echo
 echo "Building modules..."
 echo
-make -j4 ARCH=arm64 CROSS_COMPILE=/home/synthlock/toolchains/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9/bin/aarch64-linux-android- lineage_tulip_defconfig modules
+make -j4 modules
 
 echo
 echo "Getting ready to make final run!"
 echo
-make -j4 ARCH=arm64 CROSS_COMPILE=/home/synthlock/toolchains/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9/bin/aarch64-linux-android- lineage_tulip_defconfig Image.gz-dtb
+make -j4 Image.gz-dtb
 
 echo
 echo "Copying Image.gz-dtb to sombrax builder..."
@@ -25,7 +33,7 @@ cp arch/arm64/boot/Image.gz-dtb ~/sombrax/
 echo
 echo "Stripping wlan.ko module..."
 echo
-/home/synthlock/toolchains/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9/bin/aarch64-linux-android-strip --strip-debug drivers/staging/prima/wlan.ko
+$CROSS_COMPILE-strip --strip-debug drivers/staging/prima/wlan.ko
 
 echo
 echo "Copying modules to sombrax builder..."
