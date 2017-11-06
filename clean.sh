c0e33765a7f435a9a7b7ed3d010c37dafe82647b
Kevin Cai <kevincai96@gmail.com>
2017-11-06 12:34:01 -0500

    Explicitly remove wlan module

--- clean.sh
+++ clean.sh
@@ -1,4 +1,5 @@
 echo "Cleaning up..."
 echo
+rm drivers/staging/prima/wlan.ko
 make -j4 ARCH=arm64 CROSS_COMPILE=/home/synthlock/toolchains/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9/bin/aarch64-linux-android- clean
 make -j4 ARCH=arm64 CROSS_COMPILE=/home/synthlock/toolchains/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9/bin/aarch64-linux-android- mrproper
